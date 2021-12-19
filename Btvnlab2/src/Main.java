eb56ddd4167786f9e0a349c942d46bab1e74251f
ahriandcat <95160129+ahriandcat@users.noreply.github.com>
2021-12-19 16:51:48 +0700

    Update Main.java

--- Btvnlab2/src/Main.java
+++ Btvnlab2/src/Main.java
@@ -2,15 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bài 2");
-        //Bai2
-        Check check = new Check();
+        //Bài 1
         Scanner sc = new Scanner(System.in);
+        System.out.println("Bài 1");
+        Check check = new Check();
+        System.out.println("Nhập vào chuỗi 1: ");
+        String chuoi_1 = sc.nextLine();
+        System.out.println("Chuỗi 1 là: " + chuoi_1);
+        System.out.println("Nhập vào chuỗi 2: ");
+        String chuoi_2 = sc.nextLine();
+        System.out.println("Chuỗi 2 là: " + chuoi_2);
+        check.checkString(chuoi_1,chuoi_2);
+
+
+        //Bài 2
+        System.out.println("Bài 2");
         System.out.println("Nhập điểm thực hành : ");
         double a = sc.nextDouble();
         while (a>10) {
             System.out.println("Nhập lại điểm thực hành");
-             a = sc.nextDouble();
+            a = sc.nextDouble();
         }
         System.out.println("Điểm thực hành là : " + a);
         System.out.println("Nhập điểm lý thuyết : ");
@@ -25,15 +36,8 @@ public class Main {
         check.checkPoint(c);
         sc.nextLine();
 
-        //Bai1
-        System.out.println("Bài 1");
-        System.out.println("Nhập vào chuỗi 1: ");
-        String chuoi_1 = sc.nextLine();
-        System.out.println("Chuỗi 1 là: " + chuoi_1);
-        System.out.println("Nhập vào chuỗi 2: ");
-        String chuoi_2 = sc.nextLine();
-        System.out.println("Chuỗi 2 là: " + chuoi_2);
-        check.checkString(chuoi_1,chuoi_2);
+
     }
 }
+
 
