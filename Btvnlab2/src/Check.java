9e963d4c7871a73c8c2b693c41a123b9267e6b91
ahriandcat <95160129+ahriandcat@users.noreply.github.com>
2021-12-19 16:52:10 +0700

    Update Check.java

--- Btvnlab2/src/Check.java
+++ Btvnlab2/src/Check.java
@@ -1,15 +1,4 @@
-import java.util.Scanner;
-
 public class Check {
-    public void checkPoint (double c) {
-        if(c < 6) {
-            System.out.println("Bạn đã trượt");
-        }
-        else {
-            System.out.println("Bạn đã đạt");
-        }
-    }
-
     public void checkString(String chuoi_1, String chuoi_2){
         if (chuoi_1.equals(chuoi_2)) {
             System.out.println("hai chuỗi giống nhau");
@@ -24,4 +13,13 @@ public class Check {
             System.out.println("Kích thước hai chuỗi khác nhau");
         }
     }
+    public void checkPoint (double c) {
+        if(c < 6) {
+            System.out.println("Bạn đã trượt");
+        }
+        else {
+            System.out.println("Bạn đã đạt");
+        }
+    }
 }
+
