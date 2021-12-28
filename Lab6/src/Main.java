4d8798f8d9b1557135424b0b7799a31772b41c78
ahriandcat <95160129+ahriandcat@users.noreply.github.com>
2021-12-29 01:49:10 +0700

    Update Main.java

--- Lab6/src/Main.java
+++ Lab6/src/Main.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         InputProduct input = new InputProduct();
-        Scanner sc = new Scanner(System.in);
         Product[] products = input.addProduct();
         input.show(products);
     }
